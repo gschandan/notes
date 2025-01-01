a2a695ae1a75ca3baec507174db20b75fc7090f4
Gurmukh Chandan <gsc128@gmail.com>
2025-01-01 16:38:09 +0000

    notes on amortised time complexity

--- posts/.vdoc.py
+++ posts/.vdoc.py
@@ -26,6 +26,9 @@
 #
 #
 #
+nums = []
+for i in range(10):
+    nums.append(i)  # Seems O(1), but occasionally triggers resize
 #
 #
 #
@@ -35,120 +38,10 @@
 #
 #
 #
-#
-#
-#
-#
-#
-#
-def isAnagram(s: str, t: str) -> bool:
-
-    if len(s) != len(t):
-        return False
-
-    return sorted(s) == sorted(t)
-
-print("Test 1:", isAnagram("racecar", "carrace"))
-print("Test 2:", isAnagram("jar", "jam"))
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-def isAnagram(s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False
-    
-    char_count = [0] * 26
-    for i in range(len(s)):
-        char_count[ord(s[i]) - ord('a')] += 1
-        char_count[ord(t[i]) - ord('a')] -= 1
-    
-
-    return all(count == 0 for count in char_count)
-
-print("Test 1:", isAnagram("racecar", "carrace")) 
-print("Test 2:", isAnagram("jar", "jam"))          
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-from collections import defaultdict
-
-def isAnagram(s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False
-    
-    char_count = defaultdict(int)
-    
-    # Count characters in both strings
-    for s_char, t_char in zip(s, t):
-        char_count[s_char] += 1
-        char_count[t_char] -= 1
-    
-    # Check if all counts are zero
-    return all(count == 0 for count in char_count.values())
-
-# Test cases
-print("Test 1:", isAnagram("racecar", "carrace"))  # True
-print("Test 2:", isAnagram("jar", "jam"))          # False
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+from collections import Counter 
+counter = Counter()
+for char in "hello":
+    counter[char] += 1  # Usually O(1), but may trigger rehash
 #
 #
 #
